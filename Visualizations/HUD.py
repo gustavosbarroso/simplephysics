d6da72d2b4c526945d5f389827368a9b5b9ddc92
gustavosbarroso <gustavo.s.barroso@gmail.com>
2026-04-11 20:39:53 -0300

    Update HUD.py

--- Visualizations/HUD.py
+++ Visualizations/HUD.py
@@ -1,24 +1,17 @@
 import numpy as np
 from matplotlib.widgets import Slider
-#Criação de HUD
-def make_hud(fig, x=0.02, y=0.6):
+# =========================================================
+# 🔷 5. HUD
+# =========================================================
+def make_hud(fig):
     return fig.text(
-        x, y,
+        0.02, 0.6,
         "",
         fontsize=10,
-        bbox=dict(
-            boxstyle="round",
-            facecolor="white",
-            alpha=0.85
-        )
+        bbox=dict(boxstyle="round", facecolor="white", alpha=0.8)
     )
-#Atualização de HUD
-def update_hud(hud, info: dict):
-    """
-    info = {"θ": 1.2, "ω": 0.3, "E": 10}
-    """
-    text = "\n".join(
-        f"{k}: {v:.4f}" if isinstance(v, float) else f"{k}: {v}"
-        for k, v in info.items()
-    )
+
+
+def update_hud(hud, info):
+    text = "\n".join([f"{k}: {v:.3f}" for k, v in info.items()])
     hud.set_text(text)
