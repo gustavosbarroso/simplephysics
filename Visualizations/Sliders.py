c91867f0fbc44d6184ad5857a8fe2370c5a6de94
gustavosbarroso <gustavo.s.barroso@gmail.com>
2026-04-11 20:42:22 -0300

    Update Sliders.py

--- Visualizations/Sliders.py
+++ Visualizations/Sliders.py
@@ -1,32 +1,14 @@
-#Parâmetros do sistema resolvido com RK4
-params = {
-    "param1": 1.0,
-    "param2": 2.0,
-    "param3": 0.5,
-    "param4": 1.5,
-}
-# ---------------------------
-# SLIDERS
-# ---------------------------
-ax_p1 = plt.axes([0.25, 0.30, 0.65, 0.03])
-ax_p2 = plt.axes([0.25, 0.25, 0.65, 0.03])
-ax_p3 = plt.axes([0.25, 0.20, 0.65, 0.03])
-ax_p4 = plt.axes([0.25, 0.15, 0.65, 0.03])
+# =========================================================
+# 🔷 8. SLIDERS
+# =========================================================
+def make_slider(ax, label, vmin, vmax, valinit):
+    return Slider(ax, label, vmin, vmax, valinit=valinit)
 
-slider_p1 = Slider(ax_p1, 'param1', 0.1, 10, valinit=params["param1"])
-slider_p2 = Slider(ax_p2, 'param2', 0.1, 10, valinit=params["param2"])
-slider_p3 = Slider(ax_p3, 'param3', 0.1, 10, valinit=params["param3"])
-slider_p4 = Slider(ax_p4, 'param4', 0.1, 10, valinit=params["param4"])
-#Update dos sliders
-def update_sliders(val):
-    global t, sol1, sol2, sol3, sol4, params
 
-    params["param1"] = slider_p1.val
-    params["param2"] = slider_p2.val
-    params["param3"] = slider_p3.val
-    params["param4"] = slider_p4.val
+def rebuild(val):
+    global t, sol, obs, ani
 
-    t, sol1, sol2, sol3, sol4 = solve(params)
+    t, sol, obs = solve(params)
 
     ani.event_source.stop()
     ani.frame_seq = ani.new_frame_seq()
