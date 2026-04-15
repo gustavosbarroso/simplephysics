1fa0f380ecc106e89321b47cdc4d0826a99873d5
gustavosbarroso <gustavo.s.barroso@gmail.com>
2026-04-15 11:42:05 -0300

    Update Sistema_desenho.py

--- Visualizations/Sistema_desenho.py
+++ Visualizations/Sistema_desenho.py
@@ -1,13 +1,13 @@
 # =========================================================
-# 🔷 6. FIGURA + ARTISTAS
+# 🔷 5. FIGURA
 # =========================================================
 def make_figure():
     fig, ax = plt.subplots(figsize=(10, 5))
-    plt.subplots_adjust(bottom=0.3)
+    plt.subplots_adjust(left=0.25, bottom=0.3)
 
     ax.set_title("Simulação Genérica")
-    ax.set_xlabel("t ou x")
-    ax.set_ylabel("estado")
+    ax.set_xlabel("t")
+    ax.set_ylabel("x")
 
     line, = ax.plot([], [], lw=2)
 
